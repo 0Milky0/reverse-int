3f1755b5fd789bbf90fa061e451f3f345d566b87
Milana <savymila@gmail.com>
2024-08-08 23:42:30 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    let arr = Array.from(n.toString());
+    let arrReversed = arr.reverse();
+    let str = "";
+
+    for(let i = 0; i < arrReversed.length; i++) {
+        str += arrReversed[i];
+    }
+
+    return str;
 }
+
+module.exports = reverse;
